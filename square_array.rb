0f1d73454cae10170cd9816b04eab70e98913a1c
Everett <everett.bowline@gmail.com>
2018-04-10 00:25:09 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
   new_array = Array.new
+<<<<<<< HEAD
   array.each {|x| new_array << x ** 2}
+=======
+  new_array = array.each {|x| x ** 2}
+>>>>>>> d4d884726a88682cf6ff6b25e38e911e585bb74c
   new_array
 end
